2f92414ab2dfaf0dd0721c06d848a63cdf1fcd92
Your Name <you@example.com>
2024-12-22 17:15:42 +0300

    tmp

---
+++ src/main/java/gift/academic/pet/constants/GeneralConstant.java
@@ -0,0 +1,6 @@
+package gift.academic.pet.constants;
+
+public class GeneralConstant {
+    public static final String defaultPhone = "89111234567";
+    public static final String defaultPassword = "Password123";
+}
---
+++ src/main/java/gift/academic/pet/models/User.java
@@ -0,0 +1,83 @@
+package gift.academic.pet.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private Long phone;
+    private String email;
+    private String name;
+    private String registrationDate;
+    private int ordersCount;
+    private int petsCount;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public int getOrdersCount() {
+        return ordersCount;
+    }
+
+    public void setOrdersCount(int ordersCount) {
+        this.ordersCount = ordersCount;
+    }
+
+    public int getPetsCount() {
+        return petsCount;
+    }
+
+    public void setPetsCount(int petsCount) {
+        this.petsCount = petsCount;
+    }
+}
